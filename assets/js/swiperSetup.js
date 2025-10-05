a6dc594d84661ee5ef807d646c919e17782548b5
johnender <johnender@users.noreply.github.com>
2025-10-05 20:25:49 +0000

    deploy: b606f04677f9823edce4ba581e802d0c84209952

--- assets/js/swiperSetup.js
+++ assets/js/swiperSetup.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", async function () {
-  const wrapper = document.getElementById("banner-swiper-wrapper");
+  const scriptTag = document.currentScript || document.querySelector('script[src*="swiperSetup.js"]');
+  const itemFile = scriptTag?.getAttribute('data-products') || 'assets/json/products.json';
+  const swiperSufix = scriptTag?.getAttribute('swiper-sufix') || 'swiper';
+  const wrapperID = scriptTag?.getAttribute('wrapper-id') || 'item-swiper-wrapper';
+  const wrapper = document.getElementById(wrapperID);
 
   try {
-    const response = await fetch("assets/json/home-banners.json"); // adjust path if needed
-    const banners = await response.json();
+    const response = await fetch(itemFile); // adjust path if needed
+    const items = await response.json();
 
     // Generate slides
-    banners.forEach(banner => {
+    items.forEach(item => {
       const slide = document.createElement("div");
       slide.classList.add("swiper-slide");
       slide.innerHTML = `
         <div class="swiper-slide--card">
-          <button onclick="window.location.href='${banner.link}';">
-            <img src="${banner.image}" class="banner" alt="${banner.alt}">
-          </button>
+          <div onclick="window.location.href='${item.link}';">
+            <img src="${item.image}" class="item" alt="${item.alt}">
+          </div>
         </div>
       `;
       wrapper.appendChild(slide);
     });
 
     // Initialize Swiper only after slides are created
-    const swiper = new Swiper('.swiper', {
+    // eslint-disable-next-line no-unused-vars
+    const swiper = new Swiper(`.swiper-${swiperSufix}`, {
       direction: 'horizontal',
       loop: true,
       slidesPerView: 1,
@@ -29,15 +34,15 @@ document.addEventListener("DOMContentLoaded", async function () {
       grabCursor: true,
       allowTouchMove: true,
       autoplay: {
-        delay: 5000,
+        delay: 3000,
       },
       pagination: {
-        el: '.swiper-pagination',
+        el: `.swiper-pagination-${swiperSufix}`,
         clickable: true,
       },
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: `.swiper-button-next-${swiperSufix}`,
+        prevEl: `.swiper-button-prev-${swiperSufix}`,
       },
       lazy: {
         enabled: true,
@@ -48,6 +53,6 @@ document.addEventListener("DOMContentLoaded", async function () {
     });
 
   } catch (error) {
-    console.error("Error loading banner.json:", error);
+    console.error("Error loading item.json:", error);
   }
 });
